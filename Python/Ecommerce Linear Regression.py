fc457b036ea25b7fddb6fb29083ba650b5dc9d8e
Samantha Lee <60999729+samanthamirae@users.noreply.github.com>
2020-02-20 22:01:07 -0800

    Update Ecommerce Linear Regression.py

--- Python/Ecommerce Linear Regression.py
+++ Python/Ecommerce Linear Regression.py
@@ -63,11 +63,10 @@ sns.distplot(y_test - predictions, bins=50)
 
 X.columns
 df = pd.DataFrame(lm.coef_, X.columns, columns=['Coefficient'])
-
+df
 
 
 - Holding all other features fixed, a 1 unit increase in **Avg. Session Length** is associated with an **increase of 25.98 total dollars spent**.
 - Holding all other features fixed, a 1 unit increase in **Time on App** is associated with an **increase of 38.59 total dollars spent**.
 - Holding all other features fixed, a 1 unit increase in **Time on Website** is associated with an **increase of 0.19 total dollars spent**.
 - Holding all other features fixed, a 1 unit increase in **Length of Membership** is associated with an **increase of 61.27 total dollars spent**.
-df
